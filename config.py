691df214424e3acc57b09294b08225cdc27639d8
Nikita <156105775+nikita-solovyov-dev@users.noreply.github.com>
2025-09-10 16:17:54 +0300

    Added google_account_file

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 
+from pydantic import computed_field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 BASE_DIR = Path(__file__).resolve().parent
@@ -14,17 +15,23 @@ class DatabaseSettings(BaseSettings):
     host: str
     port: int
 
-    model_config = SettingsConfigDict(env_file=ENV_FILE, env_prefix="DB_")
+    model_config = SettingsConfigDict(env_file=ENV_FILE, env_prefix="DB_", extra="ignore")
 
 
 class Settings(BaseSettings):
     base_dir: Path = BASE_DIR
-    env_file: Path = ENV_FILE
     temp_dir: Path = TEMP_DIR
+    env_file: Path = ENV_FILE
+
+    google_account_file_name: str
 
     db: DatabaseSettings = DatabaseSettings()  # noqa
 
-    model_config = SettingsConfigDict(env_file=ENV_FILE)
+    model_config = SettingsConfigDict(env_file=ENV_FILE, extra="ignore")
+
+    @computed_field
+    def google_account_file(self) -> Path:
+        return self.base_dir / self.google_account_file_name
 
 
 settings = Settings()
