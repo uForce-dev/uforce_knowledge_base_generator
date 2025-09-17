e49f9700a5368ce120e5df1b38a7e05ebfc69c94
Nikita <156105775+nikita-solovyov-dev@users.noreply.github.com>
2025-09-17 11:38:17 +0300

    change mm channels

--- src/constants.py
+++ src/constants.py
@@ -1,22 +1,21 @@
 TEAMLY_EXCLUDED_ARTICLE_IDS = []
 
 MATTERMOST_CHANNEL_IDS = [
-    "z5t5hy9h6fbdjxk3eaw1qsikdo",  # General
-    "ykt6smajcp857frb1cpsmqhdpy",  # teamleaders-retro-for-team
-    "qm9swj611p8otdbhzh14on4ztw",  # help
-    "utcs94emzjdypnpuzrtkzw8n1a",  # tasks
-    "xnz3beafw3nrf81fp73coipcje",  # uChill
-    "meyhn7mq17gpfbeicirituk4te",  # uCelebrate
-    "7egtoa4z6t8rmrxcqtufytrrza",  # uLearn
-    "s4s64yy5n3f4pq1bzz5hg51qba",  # off-topic
-    "rxa4r4zocfb7u8cxw1hwax3kkw",  # marketing
-    "stgx99dyu7fr3er3igj65wg4ga",  # agreements
-    "kd48jy3h9ibnxcaf4yug9f4yfc",  # infrastructure
-    "qc6u9f65ytygdxebrfs6ff9znc",  # project-crm
-    "8ooexd4ajt8db8u7xyf6c4xdec",  # project-telemessaging
-    "d9mygh6boproxp3ynzeff3digw",  # team-performance
-    "doxebjbssbre3r1yyze5ntzhrr",  # sales
-    "c73q3ike5ffzxmgaey6qmqu7zo",  # sales-inbound-leads
+    "z5t5hy9h6fbdjxk3eaw1qsikdo",  # ~town-square
+    "ykt6smajcp857frb1cpsmqhdpy",  # ~teamleaders-retro-for-team
+    "utcs94emzjdypnpuzrtkzw8n1a",  # ~tasks
+    "rxa4r4zocfb7u8cxw1hwax3kkw",  # ~marketing
+    "stgx99dyu7fr3er3igj65wg4ga",  # ~agreements
+    "doxebjbssbre3r1yyze5ntzhrr",  # ~sales
+    "c73q3ike5ffzxmgaey6qmqu7zo",  # ~inbound
+    "iwcocsm6gpnc7fnex77a8awh7c",  # ~outbound
+    "hzmsuyno1tbdjyfi84ui4pmq5e",  # ~serm
+    "d9mygh6boproxp3ynzeff3digw",  # ~team-performance
+    "xpcpu3sgmffutkfrfz4gzah3wr",  # ~team-market-research
+    "suqtintohiyr3bt36mh3zsdjqo",  # ~leadgen-fundraising
+    "zrgtwgw8dt87tx7nbcf8d171nw",  # ~sales-tenders
+    "jddra1sbrf8qjpnxfbcdbquk5e",  # ~outreach-support-mr
+    "rt7678awpinnijupi94t7tkjxw",  # ~im
 ]
 
 HR_SPLIT_FILES_COUNT = 10
